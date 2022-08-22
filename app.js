6fcb37fb89605be77e906261245e3c229b291ec5
Freddy conto <feconto@gmail.com>
2022-08-22 12:09:19 -0500

    heroku SSL apply

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.json('Welcome to Home');
+  res.json('Welcome to Advising');
 });
 
 app.listen(port, () => {
