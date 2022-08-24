aef606c6ce3add93af4b1f87be5b535c328865fb
Freddy conto <feconto@gmail.com>
2022-08-24 11:41:28 -0500

    message add

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.json('Welcome to Advising');
+  res.json('Welcome to Advising Mongo, JWT');
 });
 
 app.use('/', require('./router/'));
